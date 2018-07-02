0ad26b45e6ea4281a4d6f964891c467db309f00b
C-Meinert <cmeinert89@gmail.com>
2018-07-02 19:01:27 -0400

    Initial project setup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KendoUtilities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
